c349db20ced29a24d23c194b3a5365e1b20f0b1e
gabrielnrt <gabrielpv98@gmail.com>
2021-07-26 22:00:10 -0300

    Começando

--- Python POO/classes.py
+++ Python POO/classes.py
@@ -1,1 +1,11 @@
 # Esse é primeiro teste tentando usar classes em python
+
+class Jogador:
+    def __init__(self, nome, idade, rating):
+        self.nome = nome
+        self.idade = idade
+        self.rating = rating
+
+a = Jogador('Kasparov', 68, 2800)
+
+print(a.rating)
